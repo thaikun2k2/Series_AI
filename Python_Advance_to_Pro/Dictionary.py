e101e413293444ff1cf7e8974c6693cdde1e927b
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2026-03-14 15:49:07 +0700

    Create Dictionary.py

---
+++ Python_Advance_to_Pro/Dictionary.py
@@ -0,0 +1,11 @@
+#kiểu dữ liệu Dictionary là một kiểu dữ liệu có thể lưu trữ nhiều giá trị khác nhau, được định nghĩa bằng cặp key-value (khóa-giá trị). Mỗi key phải là duy nhất trong dictionary và được sử dụng để truy cập giá trị tương ứng.
+#kết hợp ưu điểm giữa list và set, dictionary cho phép lưu trữ dữ liệu theo cặp key-value, trong đó key là một chuỗi hoặc một số, và value có thể là bất kỳ kiểu dữ liệu nào, bao gồm cả list hoặc dictionary khác.
+
+
+#Cách tạo một dictionary:
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+
+}
