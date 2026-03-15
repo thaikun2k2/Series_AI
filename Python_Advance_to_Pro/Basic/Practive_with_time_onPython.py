db14b5e8b3a993af940cdd2a8dcf9c1f8449c099
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2026-03-15 20:41:30 +0700

    Update Practive_with_time_onPython.py

--- Python_Advance_to_Pro/Basic/Practive_with_time_onPython.py
+++ Python_Advance_to_Pro/Basic/Practive_with_time_onPython.py
@@ -11,9 +11,41 @@ tichs = time.time()#tichs == tích tắc (đồng hồ quả lắc) = sec
 # print(type(nowtime1))
 
 nowtime = time.localtime(tichs) #outcome time ở khu vực hiện tại(eg:Việt Nam = GMT + 7 hours) ##time hiện tại 1773580299,..
-print(nowtime)
-print(type(nowtime))
+# print(nowtime)
+# print(type(nowtime))
 
-nowtime2 = time.gmtime(tichs) #outcome time year,month,day theo UTC(giờ quốc tế) = GMT
-print(nowtime2)
-print(type(nowtime2))
+# nowtime2 = time.gmtime(tichs) #outcome time year,month,day theo UTC(giờ quốc tế) = GMT
+# print(nowtime2)
+# print(type(nowtime2))
+
+## nowtime, nowtime2 trả về 1 tuple
+# chuyển sang dạng dễ đọc use: time.asctime() =>> time về string
+## lưu ý phải đưa vào dữ liệu kiểu tuple should not đưa tichs vào đc
+
+##cách 1: 
+# lấy số sec(tichs) --> nowtime(tuple)=time.localtime() -->time.asctime(nowtime) -->now time string
+timeread = time.asctime(nowtime)
+print(timeread)
+print(type(timeread))
+
+##cách 2: lấy số sec(tichs) --> time.ctime()
+timeread2 = time.ctime(tichs)
+print(timeread2)
+print(type(timeread2))
+
+
+
+
+######Date and Time: Module Time:
+# time.time() time(sec) --> use time.ctime() --> time(string)
+# time.time() time(sec) --> use time.localtime() or time.gmtime() --> time(tuple) -->use time.asctime() --> time(string)
+# time.time() time(sec) --> use time.localtime() or time.gmtime() --> time(tuple) -->use time.strftime() --> time(format)
+# time.time() time(sec) <-- use time.mktime() <-- time(tuple) <--use time.strptime() <-- time (format)
+"""
+| # | Luồng command Python                                                     | Kết quả cuối                                        |
+| - | ------------------------------------------------------------------------ | --------------------------------------------------- |
+| 1 | `time.time()` → `time.ctime()`                                           | `time(sec)` → `time(string)`                        |
+| 2 | `time.time()` → `time.localtime()` / `time.gmtime()` → `time.asctime()`  | `time(sec)` → `time(tuple)` → `time(string)`        |
+| 3 | `time.time()` → `time.localtime()` / `time.gmtime()` → `time.strftime()` | `time(sec)` → `time(tuple)` → `time(format string)` |
+| 4 | `time.strptime()` → `time.mktime()`                                      | `time(format string)` → `time(tuple)` → `time(sec)` |
+"""
